a95e35cee6f272f6e9c4f23fba1016e5fae426b9
Sunil Bishwakarma <bishwast77@gmail.com>
2021-10-10 23:33:58 -0600

    In-Progress

---
+++ SBNoteMgmtApp/Models/NoteModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SBNoteMgmtApp.Models
+{
+    public class NoteModel
+    {
+        //Following are the properties to the Note App. Allowing Client to set and get the properties.
+        public Guid Id { get; set; }
+        public string Subject { get; set; }
+        public string Details { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ SBNoteMgmtApp/Reposotory/NoteRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SBNoteMgmtApp.Models;
+
+namespace SBNoteMgmtApp.Reposotory
+{
+    public interface INoteRepository
+    {
+        void DeleteNotes(NoteModel noteModel);
+        NoteModel FindNoteById(Guid id);
+        IEnumerable<Notemodel> GetAllNotes();
+        void SaveNotes(NoteModel noteModel);
+    }
+
+    /// <summary>
+    /// This class is a container to store the notes.
+    /// </summary>
+    public class NoteRepository : INoteRepository
+    //Created a Interface to put the signature of the class. 
+    {
+        /// <summary>
+        /// Container for storing the notes.
+        /// </summary>
+        private readonly List<NoteModel> _notes;
+
+        // Following methods will help to interact with the container. 
+
+        public NoteRepository()
+        {
+            _notes = new List<NoteModel>();
+        }
+        public NoteModel FindNoteById(Guid id)
+        {
+            var note = _notes.Find(notes n => n.Id == id);
+            return note;
+        }
+        public IEnumerable<Notemodel> GetAllNotes()
+        {
+            return _notes;
+        }
+        public void SaveNotes(NoteModel noteModel)
+        {
+            _notes.Add(noteModel);
+        }
+        public void DeleteNotes(NoteModel noteModel)
+        {
+            _notes.Remove(noteModel);
+        }
+    }
+}
